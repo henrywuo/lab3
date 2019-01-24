a7a364d30792a118121362917f0d22b90afd67ff
Henry Wu Ou <hwou@ucsd.edu>
2019-01-24 10:45:23 -0800

    Lab3

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+	var naemToShow = req.params.userName;
+	if (naemToShow === undefined) {
+		nameToShow = 'World';
+	}
+	console.log("Name is " + nameToShow);
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
